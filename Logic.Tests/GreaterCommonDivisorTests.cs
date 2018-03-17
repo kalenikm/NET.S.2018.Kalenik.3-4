201dc4456ce54656ecf7aede1e57b569902f171b
kalenikm <kalenikmisha@gmail.com>
2018-03-17 18:35:34 +0300

    GCD class and test were added

---
+++ Logic.Tests/GreaterCommonDivisorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    public class GreaterCommonDivisorTests
+    {
+        [TestCase(new[] { 3, 743, 75, 3, 4, 85, 7, 65, 4, 94, 3, 6, 68, 4, 6, 372, 46 }, ExpectedResult = 1)]
+        [TestCase(new[] { 3, 12, 48, 9, 18 }, ExpectedResult = 3)]
+        [TestCase(new[] { 25, 5, 4, 8, 3246, 1, 8, 568, 56 }, ExpectedResult = 1)]
+        [TestCase(new[] { 25, 5, 100, 10, 125, 35, 15, 75, 55 }, ExpectedResult = 5)]
+        public int NormalGcdTestWithManyNumbers(int[] arr)
+        {
+            return GreaterCommonDivisor.NormalGcd(arr);
+        }
+
+        [TestCase(35, 28, ExpectedResult = 7)]
+        [TestCase(28, 13, ExpectedResult = 1)]
+        [TestCase(12, 48, ExpectedResult = 12)]
+        [TestCase(0, 48, ExpectedResult = 48)]
+        [TestCase(1, 48, ExpectedResult = 1)]
+        public int NormalGcdTestWithTwoNumbers(int a, int b)
+        {
+            return GreaterCommonDivisor.NormalGcd(a, b);
+        }
+
+        [TestCase]
+        public void NormalGcd_ThrowArgumentException_NegativeNumber()
+        {
+            Assert.Throws<ArgumentException>(() => GreaterCommonDivisor.NormalGcd(-35, 28));
+        }
+    }
+}
---
+++ Logic/GreaterCommonDivisor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Logic
+{
+    public class GreaterCommonDivisor
+    {
+        public static int NormalGcd(int a, int b)
+        {
+            if(a < 0 || b < 0)
+                throw new ArgumentException("Negative number.");
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a + b;
+        }
+
+        public static int NormalGcd(out TimeSpan time, params int[] array)
+        {
+            if(array == null || array.Length == 0)
+                throw new ArgumentException($"Array {nameof(array)} is empty.");
+
+            Stopwatch watch = new Stopwatch();
+            int[] res = new int[(array.Length + 1) / 2];
+            watch.Start();
+            while (array.Length != 1)
+            {
+                for (int i = 0; i < array.Length; i += 2)
+                {
+                    res[i / 2] = i + 1 < array.Length ? NormalGcd(array[i + 1], array[i]) : NormalGcd(array[i], array[i]);
+                    if (res[i / 2] == 1)
+                    {
+                        watch.Stop();
+                        time = watch.Elapsed;
+                        return 1;
+                    }
+                }
+                array = res;
+                res = new int[(array.Length + 1) / 2];
+            }
+            watch.Stop();
+            time = watch.Elapsed;
+            return array[0];
+        }
+
+        public static int NormalGcd(params int[] array)
+        {
+            if (array == null || array.Length == 0)
+                throw new ArgumentException($"Array {nameof(array)} is empty.");
+
+            int[] res = new int[(array.Length + 1) / 2];
+            while (array.Length != 1)
+            {
+                for (int i = 0; i < array.Length; i += 2)
+                {
+                    res[i / 2] = i + 1 < array.Length ? NormalGcd(array[i + 1], array[i]) : NormalGcd(array[i], array[i]);
+                    if (res[i / 2] == 1)
+                        return 1;
+                }
+                array = res;
+                res = new int[(array.Length + 1) / 2];
+            }
+            return array[0];
+        }
+    }
+}
